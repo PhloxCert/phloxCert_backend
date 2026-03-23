37f0b2196a5da14ff79e022e7ad24cb8d728efb1
lucacl <luca.claus02@gmail.com>
2026-03-23 20:33:35 +0100

    modified for production

--- src/server.ts
+++ src/server.ts
@@ -23,7 +23,7 @@ if (!PACKAGE_ID || !REGISTRY_ID) {
 
 const app = express();
 
-app.options('*', (req, res) => {
+app.options('(.*)', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
